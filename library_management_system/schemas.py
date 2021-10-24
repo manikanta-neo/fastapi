c1af59837162d988ee35bf127f7ad54b091ddd65
manikanta-neo <manikanta.d@neosoftmail.com>
2021-10-24 20:10:33 +0530

    24oct

--- library_management_system/schemas.py
+++ library_management_system/schemas.py
@@ -2,17 +2,37 @@ from pydantic import BaseModel
 from models import Book
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
+
 class BookAdd(BaseModel):
-    name:str
-    author:str
+    name: str
+    author: str
     description: str
     total_copies: int
-    genre:str
-    available_copies:int
+    genre: str
+    available_copies: int
+
 
 class BookModel(SQLAlchemyObjectType):
     class Meta:
         model = Book
 
+
 class BookDel(BaseModel):
-    id:int
+    id: int
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    password: str
+    book: str
+
+
+class Issue_book(BaseModel):
+    id: int
+    date_added: str
+    Issued_to: str
+    date_issued: str
+    date_return: str
+    book: str
